ba9894127808f8e85d2205251abf5656060e7b86
Amardeep Singh <30592051+Amardeep-Singh-Developer@users.noreply.github.com>
2025-11-14 17:16:48 +0530

    Update after_install.sh

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,13 +1,2 @@
 #!/bin/bash
-set -e
-
-cd /home/ec2-user/server/dist/stolt
-
-# Install dependencies
-npm install
-
-# Start Angular SSR server in background
-nohup PORT=4000 HOST=0.0.0.0 node --experimental-specifier-resolution=node server/main.server.mjs > /home/ec2-user/server/serve.log 2>&1 &
-
-echo "✅ Angular Universal SSR app started"
-exit 0
+sudo cp -r /usr/share/nginx/html/* /usr/share/nginx/html/
