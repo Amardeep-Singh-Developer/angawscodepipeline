868cc42fe22390c8673fc81dd3160682e311262c
Amardeep Singh <30592051+Amardeep-Singh-Developer@users.noreply.github.com>
2025-11-14 17:17:05 +0530

    Update app_start.sh

--- scripts/app_start.sh
+++ scripts/app_start.sh
@@ -1,13 +1,3 @@
 #!/bin/bash
-set -e
-
-cd /home/ec2-user/server/dist/stolt
-
-# Install dependencies
-npm install
-
-# Start SSR server in background
-nohup node server/main.js > /home/ec2-user/server/serve.log 2>&1 &
-
-echo "✅ Angular Universal SSR app started"
-exit 0
+sudo systemctl start nginx
+sudo systemctl enable nginx
