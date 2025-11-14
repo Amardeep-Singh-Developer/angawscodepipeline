c63259b385449942f652b37e765ce7e042650913
Amardeep Singh <30592051+Amardeep-Singh-Developer@users.noreply.github.com>
2025-11-14 17:16:33 +0530

    Update before_install.sh

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,17 +1,4 @@
 #!/bin/bash
-set -e
-
-cd /home/ec2-user/server
-
-# Clean old deployment files to avoid overwrite errors
-rm -rf *
-
-# Install correct Node.js version (v20)
-sudo yum remove -y nodejs npm
-curl -fsSL https://rpm.nodesource.com/setup_20.x | sudo -E bash -
-sudo yum install -y nodejs
-
-# Install PM2 globally
-sudo npm install pm2@latest -g
-
-echo "✅ EC2 prepared: old files cleaned & Node v20 installed."
+sudo yum install -y nginx
+sudo systemctl stop nginx
+sudo rm -rf /usr/share/nginx/html/*
